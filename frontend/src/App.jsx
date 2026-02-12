ddfeaec073aeac5a5e51453982f155106d2b462c
Diallo_Codeur <mouhammadsitadiallo@gmail.com>
2026-02-12 19:34:03 +0100

    Frontend folder created

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+//TODO: Install all tools i need to begin the project such as express, Mongoose, ... and
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div class="bg-cyan-500">
+        <h1 class="text-3xl font-bold">
+          DC EXPENSE TRACKER
+        </h1>
+      </div>
+    </>
+  )
+}
+
+export default App 
